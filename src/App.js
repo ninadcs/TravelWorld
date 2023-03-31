ae4a124bcb18732af414e1a5a9721dce715ab82f
NilsMist <118739524+NilsMist@users.noreply.github.com>
2023-03-31 04:59:17 +0100

    Add files via upload
    Contact us page first attempt

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import SearchItem from "./components/searchItem/SearchItem";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
-import Pricing from "./pages/pricing/Pricing";
-import SignInSide from "./pages/SignIn/SignInSide";
-import SignUp from "./pages/SignUp/SignUp";
+import React from "react";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
-        <Route path="/travelworld/signin" element={<SignInSide/>}/>
-        <Route path="/travelworld/signup" element={<SignUp/>}/>
-        
-      </Routes>
-    </BrowserRouter>
+      <>
+      <Form/>
+      </>
   );
+    
 }
 
 export default App;
