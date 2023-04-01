96435526b1e13285f8331e8c5b09d130de7b1f4d
nipun <220298344@aston.ac.uk>
2023-04-01 16:45:13 +0100

    front end with api requests

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+      <ul className="fList">
+          <li className="fListItem">About Us </li>
+               
+        </ul>
+
+        <ul className="fList">
+        <li className="fListItem">Contact us</li>
+               
+        </ul>
+        <ul className="fList">
+        <li className="fListItem">Facebook</li>
+               
+        </ul><ul className="fList">
+        <li className="fListItem">Instagram</li>
+               
+        </ul>
+        
+      </div>
+     
+    </div>
+  );
+};
+
+export default Footer;
