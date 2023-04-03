8acbcfcd6ab20e47afd4b3eea8735ef6a31549f2
NilsMist <118739524+NilsMist@users.noreply.github.com>
2023-04-03 11:43:02 +0100

    Contact Us and About Us pages
    This is the final contact and about us page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import Form from "./components/Form";
+import React from "react"
+import Form from "./components/Form"
+import About from "./components/About"
 
 function App() {
   return (
-      <>
-      <Form/>
-      </>
+    <>
+    {/*<Form/>*/}
+    <About/>
+    </>
   );
-    
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState} from 'react'
 
 const Form = () => {
-        const[data, setData] = useState ({name: "", email: "", phone: "", message: ""});
-        const handleChange = (e) =>{
-            const name=e.target.name;
-            const value= e.target.value;
-            setData({...data, [name]: value})
+    const[data, setData] = useState({name:"", email:"", phone:"", message:""});
+    const handleChange = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setData({...data, [name]: value})
+    }
 
-        }
-        const handleSubmit =(e)=>{
-            e.preventDefault()
-            alert(data)
-
-        }
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        alert(data)
+    }
     return (
-        <Form method='post' onSubmit={handleSubmit}>
-            <h1>Contact<span> Us</span></h1>
+        <form method='post' onSubmit={handleSubmit}>
+            <h1>Contact <span>Us</span></h1>
+            <p> Please fill out this form if you have any queries, or contact us on our social media! </p>
             <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter Name'/>
-            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com' />
+            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com'/>
             <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+44'/>
-            <textarea name ="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder='Please Type here...'/>
-            <button type='submit' > send</button>
-            <p> {data.name}, {data.email}, {data.phone}, {data.message}</p>
-        </Form>
+            <textarea name ="message" id="" cols="30" onChange={handleChange} value={data.message} placeholder='Type here...'/>
+            <button type='submit'>send</button>
+            {/*<p>{data.name}, {data.email}, {data.phone}, {data.message}</p>*/}
+        </form>
 
     )
 }
 
-export default Form;
+export default Form
